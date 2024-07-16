fdc4ee07cf2c03a4cf5517a1e32b8b760a4a9588
Jeiberth Malagon <136020909+Jeiberth@users.noreply.github.com>
2024-07-16 14:26:05 -0400

    Update random_advice.php

---
+++ apis/random_advice.php
@@ -0,0 +1,26 @@
+<?php
+header('Content-Type: application/json');
+
+// Fetch random advice
+$adviceResponse = file_get_contents('https://api.adviceslip.com/advice');
+$adviceData = json_decode($adviceResponse, true);
+$advice = $adviceData['slip']['advice'];
+
+// Fetch related photo
+$apiKey = 'YOUR_PEXELS_API_KEY';
+$photoResponse = file_get_contents("https://api.pexels.com/v1/search?query=" . urlencode($advice) . "&per_page=1", false, stream_context_create([
+    "http" => [
+        "header" => "Authorization: $apiKey"
+    ]
+]));
+$photoData = json_decode($photoResponse, true);
+$photoUrl = $photoData['photos'][0]['src']['medium'] ?? '';
+
+// Combine advice and photo
+$response = [
+    'advice' => $advice,
+    'photo' => $photoUrl
+];
+
+echo json_encode($response);
+?>
